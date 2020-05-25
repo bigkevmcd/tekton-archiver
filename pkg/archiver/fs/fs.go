16fd16f2d2f0fbf0a64871765a816d0b0590e53b
Kevin McDermott <bigkevmcd@gmail.com>
2020-05-25 13:24:25 +0100

    Separate log extraction and archiving.

--- pkg/archiver/fs/fs.go
+++ pkg/archiver/fs/fs.go
@@ -2,57 +2,34 @@ package fs
 
 import (
 	"context"
-	"encoding/json"
 	"path/filepath"
 
 	"github.com/spf13/afero"
-
-	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
-
-	"github.com/bigkevmcd/tekton-archiver/pkg/logs"
 )
 
-// FsArchiver implements the LogArchiver interface, and archives to a specific
+// FsArchiver implements the Archiver interface, and archives to a specific
 // filesystem path.
 //
 // This is useful for testing.
 type FsArchiver struct {
 	fs       afero.Fs
 	basePath string
-	extract  logs.Extractor
 }
 
 // New creates and returns a new FsArchiver.
-func New(fs afero.Fs, e logs.Extractor, basePath string) *FsArchiver {
-	return &FsArchiver{fs: fs, extract: e, basePath: basePath}
+func New(fs afero.Fs, basePath string) *FsArchiver {
+	return &FsArchiver{fs: fs, basePath: basePath}
 }
 
-func (a *FsArchiver) ArchivePipelineRun(ctx context.Context, pr *pipelinev1.PipelineRun) ([]string, error) {
-	l, err := a.extract.PipelineRun(ctx, pr)
-	if err != nil {
-		return nil, err
-	}
+func (a *FsArchiver) Archive(ctx context.Context, logs map[string][]byte) ([]string, error) {
 	written := []string{}
-	for k, v := range l {
-		fname := pathForPR(a.basePath, pr, k+".txt")
+	for k, v := range logs {
+		fname := filepath.Join(a.basePath, k)
 		err := afero.WriteFile(a.fs, fname, v, 0644)
 		if err != nil {
 			return nil, err
 		}
 		written = append(written, fname)
 	}
-
 	return written, nil
-}
-
-func (a *FsArchiver) ArchiveTaskRun(ctx context.Context, tr *pipelinev1.TaskRun) ([]string, error) {
-	return []string{}, nil
-}
-
-func pathForPR(base string, p *pipelinev1.PipelineRun, elements ...string) string {
-	return filepath.Join(append([]string{base, p.ObjectMeta.Name}, elements...)...)
-}
-
-func metadata(pr *pipelinev1.PipelineRun) ([]byte, error) {
-	return json.Marshal(pr)
 }
--- pkg/archiver/fs/fs_test.go
+++ pkg/archiver/fs/fs_test.go
@@ -6,23 +6,28 @@ import (
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/spf13/afero"
-	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 
-	"github.com/bigkevmcd/tekton-archiver/pkg/logs"
-	"github.com/bigkevmcd/tekton-archiver/test"
+	"github.com/bigkevmcd/tekton-archiver/pkg/archiver"
 )
 
-func TestArchivingPipelineRun(t *testing.T) {
-	fs := afero.NewMemMapFs()
-	a := New(fs, &mockExtractor{}, "/tmp/logs")
-	pr := test.MakePipelineRun("my-test-pr", "my-test-pr-pod-12345")
+var _ archiver.Archiver = (*FsArchiver)(nil)
 
-	paths, err := a.ArchivePipelineRun(context.TODO(), pr)
+func TestArchive(t *testing.T) {
+	fs := afero.NewMemMapFs()
+	a := New(fs, "/tmp/logs")
+
+	paths, err := a.Archive(context.TODO(), map[string][]byte{
+		"my-test-pr/my-test-pr-pod-12345.txt": []byte("test-output"),
+		"my-test-pr/my-test-pr-pod-23456.txt": []byte("other-output"),
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	want := []string{"/tmp/logs/my-test-pr/my-test-pr-pod-12345.txt"}
+	want := []string{
+		"/tmp/logs/my-test-pr/my-test-pr-pod-12345.txt",
+		"/tmp/logs/my-test-pr/my-test-pr-pod-23456.txt",
+	}
 	if diff := cmp.Diff(want, paths); diff != "" {
 		t.Fatalf("failed to archive paths:\n%s", diff)
 	}
@@ -30,22 +35,10 @@ func TestArchivingPipelineRun(t *testing.T) {
 	if string(b) != "test-output" {
 		t.Fatalf("got %s, want %s", string(b), "test-output")
 	}
-}
-
-var _ logs.Extractor = (*mockExtractor)(nil)
-
-type mockExtractor struct {
-}
-
-func (m *mockExtractor) PipelineRun(ctx context.Context, pr *pipelinev1.PipelineRun) (map[string][]byte, error) {
-	data := map[string][]byte{
-		"my-test-pr-pod-12345": []byte("test-output"),
+	b = mustReadFile(t, fs, "/tmp/logs/my-test-pr/my-test-pr-pod-23456.txt")
+	if string(b) != "other-output" {
+		t.Fatalf("got %s, want %s", string(b), "other-output")
 	}
-	return data, nil
-}
-
-func (m *mockExtractor) TaskRun(ctx context.Context, pr *pipelinev1.TaskRun) ([]byte, error) {
-	return nil, nil
 }
 
 func mustReadFile(t *testing.T, fs afero.Fs, fname string) []byte {
